77e1bf69db0152c76e76b103989db84030186b66
Noormohamed  A <136800904+Azard2026@users.noreply.github.com>
2023-12-20 11:57:03 +0530

    Update urls.py

--- bookshop/urls.py
+++ bookshop/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('bookdetails.urls')),
 ]
-urlpatterns += staticfiles_urlpatterns
+urlpatterns += staticfiles_urlpatterns()
 
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
